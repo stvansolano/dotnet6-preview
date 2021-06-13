cda5a72117461cb46b187eac0010c803a07e973e
Codespaces <codespaces@github.com>
2021-06-13 04:52:24 +0000

    Codespaces

--- src/WebApi-Swagger/Program.cs
+++ src/WebApi-Swagger/Program.cs
@@ -1,23 +1,26 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
-
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
 app.MapControllers();
 
 app.Run();
