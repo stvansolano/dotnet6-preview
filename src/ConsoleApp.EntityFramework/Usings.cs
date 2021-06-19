8b2a8cc96e383f61a627c0fe55e4480882ac697b
Esteban Solano Granados <stvansolano@outlook.com>
2021-06-19 16:13:49 +0000

    Added EF sample

---
+++ src/ConsoleApp.EntityFramework/Usings.cs
@@ -0,0 +1,7 @@
+global using System;
+global using System.Linq;
+global using System.Collections.Generic;
+
+global using MyCode;
+
+// dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 6.0.0-preview.5.21301.9
