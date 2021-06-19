904aa860541a51f661a885362df988db154c0b77
Esteban Solano Granados <stvansolano@outlook.com>
2021-06-19 16:33:18 +0000

    Added SQLite sample

--- src/ConsoleApp.EntityFramework/Program.cs
+++ src/ConsoleApp.EntityFramework/Program.cs
@@ -1,1 +1,6 @@
-﻿WriteLine("Hello World!");
+﻿using MyConsole = MyCode.Functions;
+var WriteLine = new Action<IEnumerable<int>>(MyConsole.WriteLine);
+
+using var myContext = new BloggingContext();
+
+Console.WriteLine(myContext.Blogs.Count());
--- src/ConsoleApp.EntityFramework/Usings.cs
+++ src/ConsoleApp.EntityFramework/Usings.cs
@@ -4,4 +4,10 @@ global using System.Collections.Generic;
 
 global using MyCode;
 
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Sqlite;
+
 // dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 6.0.0-preview.5.21301.9
+// dotnet tool install --global dotnet-ef --version 6.0.0-preview.5.21301.9
+
+// dotnet add package Microsoft.EntityFrameworkCore.Sqlite.Core --version 6.0.0-preview.5.21301.9
