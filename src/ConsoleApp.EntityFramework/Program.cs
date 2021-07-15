e1a82d99160f103464e0bdc8cc3376ed7442e454
Esteban Solano Granados <stvansolano@outlook.com>
2021-07-15 05:25:00 +0000

    Updaated EntityFramework sample

--- src/ConsoleApp.EntityFramework/Program.cs
+++ src/ConsoleApp.EntityFramework/Program.cs
@@ -1,9 +1,15 @@
 ﻿using static System.Console;
 
 using var myContext = new BloggingContext();
+myContext.Database.EnsureDeleted();
 myContext.Database.EnsureCreated();
 
-myContext.Blogs.Add(new Blog{ Url = "https://stvansolano.github.io/blog" });
+var stvansolano = new Blog { Url = "https://stvansolano.github.io/blog"};
+stvansolano.Posts.Add(new Post { Title = "Hello, World!", Content = "This is my first post!"});
+
+myContext.Blogs.Add(stvansolano);
 
 await myContext.SaveChangesAsync();
-WriteLine(myContext.Blogs.Count());
+
+WriteLine($"Blogs: {myContext.Blogs.Count()}");
+WriteLine($"Blog: {myContext.Blogs.First()?.Url} -> {myContext.Blogs.First().Posts.First()?.Content}");
